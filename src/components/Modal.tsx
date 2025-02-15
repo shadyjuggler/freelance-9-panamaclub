286a4f43bf6fbc4e399bf0bdb39deced9c4470b1
Alex <yuriev200@gmail.com>
2025-02-15 21:40:08 +0200

    fixes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,7 +4,7 @@ import SuccessSvg from "../assets/success.svg";
 
 export const Modal = ({ isHidden, toggle }) => {
 
-    return isHidden ?
+    return !isHidden ?
         (
             <div className="flex absolute top-0 left-0 w-screen h-screen justify-center items-center" style={{background: "rgba(0,0,0,10%)"}}>
                 <div className="bg-white rounded-xl max-w-lg w-full py-11 flex flex-col items-center shadow">
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -4,13 +4,17 @@ import { Navigation } from "../components/Navigation"
 import { Cabinet } from "../components/Cabinet"
 import { UserRegister } from "../components/UserRegister"
 import { UserSettings } from "../components/UserSettings"
+import { Withdraw } from "../components/Withdraw"
+import { Deposit } from "../components/Deposit"
 
 export const UserPage = () => {
     return (
         <div className="w-full flex h-screen bg-slate-100">
             <Navigation/>
             {/* <Cabinet/> */}
-            <UserSettings/>
+            {/* <UserSettings/> */}
+            {/* <Deposit/> */}
+            <Withdraw/>
             {/* <UserRegister/> */}
         </div>
     )
